fcd3d91ef52529180698a01cfbbe430cbecfd39a
Prithviraj Chavan <lenskart271@gmail.com>
2026-04-30 17:15:53 +0530

    profile page to be built

---
+++ src/app/components/profile-page/profile-page.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-profile-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile-page.component.html',
+  styleUrl: './profile-page.component.scss'
+})
+export class ProfilePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
+
+  goBack() {
+  window.history.back();
+}
+
+}
