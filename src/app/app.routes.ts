f706bf20f9dd0ba3acbeb9cbaff8131fb4491c30
Prithviraj Chavan <lenskart271@gmail.com>
2026-05-05 14:30:55 +0530

    formatted the code

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -19,55 +19,53 @@ import { ProfilePageComponent } from './components/profile-page/profile-page.com
 
 export const routes: Routes = [
 
-    {path: '', component: LoginPageComponentComponent},
-    {path: 'signup', component: RegisterComponent},
+    { path: '', component: LoginPageComponentComponent },
+    { path: 'signup', component: RegisterComponent },
 
-{path:'',component:MainComponentComponent,children:[
-    {path: 'manage-expense', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'employee' }},
-    {path: 'review-expense', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'employee' }},
+    {
+        path: '', component: MainComponentComponent, children: [
+            { path: 'manage-expense', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'employee' } },
+            { path: 'review-expense', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'employee' } },
 
-    // {path: 'add-expense2', component: AddExpensePageComponent, canActivate: [authGuard], data: { role: 'Employee' }},
-    {path: 'user-dashboard', component: UserDashboardComponentComponent, canActivate: [authGuard], data: { role: 'employee' }},
-    // {path: 'user-dashboard', component: UserDashboardComponentComponent, canActivate: [authGuard], data: { role: 'employee' }},
-    
-    // {path: 'visits', component: VisitsTableComponent, canActivate: [authGuard], data: { role: 'employee' }},
-    {path: 'visits', component: VisitsTableComponent},
-    
-    {path: 'add-visit', component: AddVisitComponent, canActivate: [authGuard], data: { role: 'employee' }},
-    {path: 'add-expense', component: AddExpenseFormComponent, canActivate: [authGuard]},
-    {path: 'expense-preview', component: ExpensePreviewPageComponent, canActivate: [authGuard]},
-    
+            // {path: 'add-expense2', component: AddExpensePageComponent, canActivate: [authGuard], data: { role: 'Employee' }},
+            { path: 'user-dashboard', component: UserDashboardComponentComponent, canActivate: [authGuard], data: { role: 'employee' } },
+            // {path: 'user-dashboard', component: UserDashboardComponentComponent, canActivate: [authGuard], data: { role: 'employee' }},
 
+            // {path: 'visits', component: VisitsTableComponent, canActivate: [authGuard], data: { role: 'employee' }},
+            { path: 'visits', component: VisitsTableComponent },
 
-
-    
-
-
-    
-
-    {path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    // {path: 'users', component: UserListComponent},
-    // {path: 'admin-add-expense', component: AddExpensePageComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    // {path: 'admin-manage-expense', component: ExpenseTableComponent},
-    {path: 'admin-review-expense', component: ExpenseReviewComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    {path: 'user-list', component: UserListComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    {path: 'user-expense-review', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    {path: 'clients', component: ClientComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-
-    {path: 'profile-page', component: ProfilePageComponent, canActivate: [authGuard]},
+            { path: 'add-visit', component: AddVisitComponent, canActivate: [authGuard], data: { role: 'employee' } },
+            { path: 'add-expense', component: AddExpenseFormComponent, canActivate: [authGuard] },
+            { path: 'expense-preview', component: ExpensePreviewPageComponent, canActivate: [authGuard] },
 
 
 
-    // {path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    // {path: 'users', component: UserListComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    // // {path: 'admin-add-expense', component: AddExpensePageComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    // // {path: 'admin-manage-expense', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    // {path: 'admin-review-expense', component: ExpenseReviewComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    // {path: 'user-list', component: UserListComponent, canActivate: [authGuard], data: { role: 'Admin' }},
-    // {path: 'user-expense-review', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'Admin' }}
 
 
 
+
+
+
+            { path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [authGuard], data: { role: 'Admin' } },
+            // {path: 'users', component: UserListComponent},
+            // {path: 'admin-add-expense', component: AddExpensePageComponent, canActivate: [authGuard], data: { role: 'Admin' }},
+            // {path: 'admin-manage-expense', component: ExpenseTableComponent},
+            { path: 'admin-review-expense', component: ExpenseReviewComponent, canActivate: [authGuard], data: { role: 'Admin' } },
+            { path: 'user-list', component: UserListComponent, canActivate: [authGuard], data: { role: 'Admin' } },
+            { path: 'user-expense-review', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'Admin' } },
+            { path: 'clients', component: ClientComponent, canActivate: [authGuard], data: { role: 'Admin' } },
+
+            { path: 'profile-page', component: ProfilePageComponent, canActivate: [authGuard] },
+
+
+
+            // {path: 'admin-dashboard', component: AdminDashboardComponent, canActivate: [authGuard], data: { role: 'Admin' }},
+            // {path: 'users', component: UserListComponent, canActivate: [authGuard], data: { role: 'Admin' }},
+            // // {path: 'admin-add-expense', component: AddExpensePageComponent, canActivate: [authGuard], data: { role: 'Admin' }},
+            // // {path: 'admin-manage-expense', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'Admin' }},
+            // {path: 'admin-review-expense', component: ExpenseReviewComponent, canActivate: [authGuard], data: { role: 'Admin' }},
+            // {path: 'user-list', component: UserListComponent, canActivate: [authGuard], data: { role: 'Admin' }},
+            // {path: 'user-expense-review', component: ExpenseTableComponent, canActivate: [authGuard], data: { role: 'Admin' }}
 
 
 
@@ -87,39 +85,43 @@ export const routes: Routes = [
 
 
 
-//     {
-//         path: 'employee',
-//         canActivate: [authGuard],
-//         data: {role: ['Employee']},
-//         children:[
-//             {path: 'manage-expense', component: ExpenseTableComponent},
-//             {path: 'add-expense', component: AddExpensePageComponent},
-//             {path: 'add-expense2', component: AddExpenseFormComponent},
-//             {path: 'review-expense', component: ExpenseTableComponent},
-//             {path: 'user-dashboard', component: UserDashboardComponentComponent},
-//             {path: 'visits', component: VisitsTableComponent}
-//         ]
-//     },
-
-//     //Shared routes
-//     {path: 'expense-preview', component: ExpensePreviewPageComponent},
-
-//     //Admin routes
-//     {
-//         path: 'admin',
-//         canActivate: [authGuard],
-//         data: {role: ['Admin']},
-//         children:[
-//             {path: 'admin-dashboard', component: AdminDashboardComponent},
-//             {path: 'users', component: UserListComponent},
-//             {path: 'admin-add-expense', component: AddExpensePageComponent},
-//             {path: 'admin-manage-expense', component: ExpenseTableComponent},
-//             {path: 'admin-review-expense', component: UserListComponent},
-//             {path: 'user-expense-review', component: ExpenseTableComponent}
-// ]}
 
 
 
+            //     {
+            //         path: 'employee',
+            //         canActivate: [authGuard],
+            //         data: {role: ['Employee']},
+            //         children:[
+            //             {path: 'manage-expense', component: ExpenseTableComponent},
+            //             {path: 'add-expense', component: AddExpensePageComponent},
+            //             {path: 'add-expense2', component: AddExpenseFormComponent},
+            //             {path: 'review-expense', component: ExpenseTableComponent},
+            //             {path: 'user-dashboard', component: UserDashboardComponentComponent},
+            //             {path: 'visits', component: VisitsTableComponent}
+            //         ]
+            //     },
 
-]}
+            //     //Shared routes
+            //     {path: 'expense-preview', component: ExpensePreviewPageComponent},
+
+            //     //Admin routes
+            //     {
+            //         path: 'admin',
+            //         canActivate: [authGuard],
+            //         data: {role: ['Admin']},
+            //         children:[
+            //             {path: 'admin-dashboard', component: AdminDashboardComponent},
+            //             {path: 'users', component: UserListComponent},
+            //             {path: 'admin-add-expense', component: AddExpensePageComponent},
+            //             {path: 'admin-manage-expense', component: ExpenseTableComponent},
+            //             {path: 'admin-review-expense', component: UserListComponent},
+            //             {path: 'user-expense-review', component: ExpenseTableComponent}
+            // ]}
+
+
+
+
+        ]
+    }
 ];
