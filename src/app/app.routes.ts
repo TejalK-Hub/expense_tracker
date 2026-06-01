1423a12139901ccd8f594add39fc30612140df10
Prithviraj Chavan <lenskart271@gmail.com>
2026-06-01 18:42:42 +0530

    no major updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -15,7 +15,7 @@ import { UserListComponent } from './components/admin/users-component/user-list/
 import { ExpenseReviewComponent } from './components/admin/expense-review/expense-review.component';
 import { AdminDashboardComponent } from './components/admin/admin-dashboard/admin-dashboard.component';
 import { ClientComponent } from './components/admin/client/client.component';
-import { ProfilePageComponent } from './components/profile-page/profile-page.component';
+import { ProfilePageComponent } from './components/shared/profile-page/profile-page.component';
 
 export const routes: Routes = [
 
