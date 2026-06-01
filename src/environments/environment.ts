04b28d8468f2cc5b6f78da7044664a072473b89c
Prithviraj Chavan <lenskart271@gmail.com>
2026-06-01 18:36:54 +0530

    shifted to localhost

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
-const api = '192.168.0.104';
+// const api = '192.168.0.104';
+// const api = '10.21.21.141';
+
 // const api = '100.105.12.55';
-// const api = 'localhost'
+const api = 'localhost'
 
 export const environment = {
   production: false,
